948041383e5d6aab367991741466dfcb091b580e
manavpethe <coderalien180@gmail.com>
2026-03-14 21:43:22 +0530

    commit

---
+++ PatternOne/src/com/app/Pattern.java
@@ -0,0 +1,49 @@
+package com.app;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+
+        int n = 5;
+
+        // Upper part
+        for (int i = 1; i <= n; i++) {
+
+            // spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            // stars and spaces
+            for (int j = 1; j <= (2 * i - 1); j++) {
+
+                if (j == 1 || j == (2 * i - 1))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--) {
+
+            // spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // stars and spaces
+            for (int j = 1; j <= (2 * i - 1); j++) {
+
+                if (j == 1 || j == (2 * i - 1))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
